ec5f258bfcd19c790ceaaaaabb421185028569be
Sabrina Mason <fireflyy85@gmail.com>
2017-07-11 22:24:38 -0400

    first commit

---
+++ Lab 12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            interface ISampleInterface
+        {
+            void SampleMethod();
+        }
+
+        class ImplementationClass : ISampleInterface
+        {
+            // Explicit interface member implementation: 
+            void ISampleInterface.SampleMethod()
+            {
+                // Method implementation.
+            }
+
+            static void Main()
+            {
+                // Declare an interface instance.
+                ISampleInterface obj = new ImplementationClass();
+
+                // Call the member.
+                obj.SampleMethod();
+            }
+    }
+}
