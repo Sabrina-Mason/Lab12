94047f44fdc1d8fd57f382a57c9dc09fe2110d53
fireflyy85 <fireflyy85@gmail.com>
2017-07-12 21:48:51 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rock  r = new Rock();
+            Sciss s = new Sciss();
+            Paper p = new Paper();
+
+            Console.WriteLine("Enter a name and select an opponent: ");
+
+            interface IGame
+        {
+            string ChooseRock();
+            string ChooseScis();
+            string ChoosePaper();
+
+            class Rock : IGame
+            {
+                public string ChooseRock();
+                {
+                    return "Rock";
+                }
+            public string Scis;
+            {
+
+             return "Scissors";
+
+
+        
+        }
+           
+
+
+        }
+    }
+}
